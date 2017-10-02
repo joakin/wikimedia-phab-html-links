16a3a47d6c2eeccb254a36f7642460f44a93534c
joakin <joaquin@chimeces.com>
2017-10-02 11:18:51 +0200

    Open links in new tab

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import * as Elm from "./Main.elm";
 const root = document.getElementById("root");
 const app = Elm.Main.embed(root, { text: getTextParam(window.location.hash) });
 
+openLinksInNewTab(root)
+
 app.ports.replaceURL.subscribe(url =>
   window.history.replaceState(null, null, url)
 );
@@ -16,4 +18,12 @@ function getTextParam(hash) {
   if (!hash) return "";
 
   return decodeURIComponent(hash.slice(1));
+}
+
+function openLinksInNewTab(el) {
+  document.body.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'A') {
+      event.target.setAttribute('target', '_blank')
+    }
+  })
 }
